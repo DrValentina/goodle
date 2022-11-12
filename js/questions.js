44b793cdfe0d220fe308c313141be38f43fca9de
DrValentina <valya_dragunova@mail.ru>
2022-11-12 18:24:16 +0300

    Правки questions

--- js/questions.js
+++ js/questions.js
@@ -1,9 +1,15 @@
 let question = document.querySelectorAll(".questions__box")
 let questionButton = document.querySelectorAll(".questions__question-btn");
 let answer = document.querySelectorAll(".questions__answer");
+let questionImg = document.querySelectorAll(".questions__question-img");
 
 for(let i = 0; i <= question.length; i++) {
   questionButton[i].addEventListener("click", function(){
     answer[i].classList.toggle("visually-hidden");
+    if(!answer[i].classList.contains("visually-hidden")){
+      questionImg[i].style.transform = "rotate(180deg)";
+    } else {
+      questionImg[i].style.transform = "rotate(0deg)";
+    }
   })
 }
